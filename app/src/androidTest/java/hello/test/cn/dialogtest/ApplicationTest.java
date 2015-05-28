0a52ae3e643975727bf28f9dc0d58fc35f6b71d9
zlchen <zhaolong_chen@kingdee.com>
2015-05-28 14:29:11 +0800

    首次提交

---
+++ app/src/androidTest/java/hello/test/cn/dialogtest/ApplicationTest.java
@@ -0,0 +1,18 @@
+package hello.test.cn.dialogtest;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+
+    }
+    public void testAdd()
+    {
+        assertEquals(1,9);
+    }
+}
---
+++ app/src/main/java/hello/test/cn/dialogtest/MyView.java
@@ -0,0 +1,53 @@
+package hello.test.cn.dialogtest;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by JornyChen
+ * Email: zhaolong_chen@kingdee.com
+ * Date: 15-5-25.
+ * Project dialogTest
+ */
+public class MyView extends View {
+    private ProgreeDrawable mProgreeDrawable;
+
+    public MyView(Context context) {
+        super(context);
+        mProgreeDrawable = new ProgreeDrawable(context);
+        setBackgroundDrawable(mProgreeDrawable);
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mProgreeDrawable = new ProgreeDrawable(context);
+        setBackgroundDrawable(mProgreeDrawable);
+    }
+
+    public MyView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mProgreeDrawable = new ProgreeDrawable(context);
+        setBackgroundDrawable(mProgreeDrawable);
+    }
+
+    public void startAnimator() {
+        if (mProgreeDrawable != null) {
+            mProgreeDrawable.startAnimator();
+        }
+    }
+
+    public void startCheck() {
+        if (mProgreeDrawable != null) {
+            mProgreeDrawable.startCheck();
+        }
+
+    }
+
+    public void finishCheck() {
+        if (mProgreeDrawable != null) {
+            mProgreeDrawable.finish();
+        }
+    }
+
+}
